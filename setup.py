9c07424c759f401d71efd2c3e53336488d8e4d8f
Harsh <h.x.dev@outlook.com>
2020-05-04 09:55:47 -0400

    Update non-critical dependencies.

--- setup.py
+++ setup.py
@@ -10,11 +10,17 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', ]
+requirements = [
+    'Click>=7.1.2',
+    ]
 
-setup_requirements = ['pytest-runner', ]
+setup_requirements = [
+    'pytest-runner',
+    ]
 
-test_requirements = ['pytest>=3', ]
+test_requirements = [
+    'pytest>=3',
+    ]
 
 setup(
     author="Harsh Parekh",
